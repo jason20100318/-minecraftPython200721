5f3985472627b28d6717f21924a1f03498ad9e9c
jason20100318 <68579356+jason20100318@users.noreply.github.com>
2020-07-21 16:45:23 +0800

    Add files via upload

---
+++ day2-7py.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 15:08:53 2020
+
+@author: user
+"""
+
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create()
+
+
+username=input("請輸入名字:")
+
+while True:
+    message=input("輸入訊息:")
+
+mc.postToChat("["+username+"]"+message)
